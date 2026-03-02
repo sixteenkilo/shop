1428251e99afc41eac83265a57c7eb1fc1dee632
sixteenkilo <sixteenkilo16k@gmail.com>
2026-03-02 23:32:54 +0500

    chore(repo): configure prettier formatting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get('PORT')) || 3001;
+  console.log(port === 3000 ? '.env positive' : '.env negative');
+  await app.listen(port);
 }
 bootstrap();
