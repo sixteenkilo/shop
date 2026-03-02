cb89c3ee6adb1fc8b3d373cda42e8206cde60a05
mrTebrom <mr.tebron@gmail.com>
2026-03-03 00:45:23 +0500

    chore(backend): setup database and finalize prisma integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
