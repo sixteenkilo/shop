d84c11b418971db7e16b408aaeecb1c72ed43825
mrTebrom <mr.tebron@gmail.com>
2026-03-03 09:39:35 +0500

    chore(backend): add global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = Number(configService.get('PORT')) || 3001;
+  app.use(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   console.log(port === 3000 ? '.env positive' : '.env negative');
   await app.listen(port);
 }
