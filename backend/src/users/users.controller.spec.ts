d00744475b4af29466d4c1bf36d8e22530aed991
sixteenkilo <sixteenkilo16k@gmail.com>
2026-03-09 02:59:46 +0500

    chore(backend): add users controller tests

---
+++ backend/src/users/users.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+
+  const mockUsersService = {
+    getUsers: jest.fn(),
+    getById: jest.fn(),
+    createUser: jest.fn(),
+    updateUser: jest.fn(),
+    destroy: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
