6339f54f4fd6593c1e04f8033eb42c6082f973ee
sixteenkilo <sixteenkilo16k@gmail.com>
2026-03-09 00:16:28 +0500

    test(roles): add unit tests for roles controller

---
+++ backend/src/roles/roles.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { RolesController } from './roles.controller';
+import { RolesService } from './roles.service';
+
+describe('RolesController', () => {
+  let controller: RolesController;
+
+  const mockRolesService = {
+    getRoles: jest.fn(),
+    getById: jest.fn(),
+    createRole: jest.fn(),
+    updateRole: jest.fn(),
+    destroy: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RolesController],
+      providers: [
+        {
+          provide: RolesService,
+          useValue: mockRolesService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<RolesController>(RolesController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
